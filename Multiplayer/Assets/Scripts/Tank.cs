dc47178a82ac3d6496d46eaa91c65f88c80ec8de
Karol Sobanski <sobanskikarol91@gmail.com>
2019-12-26 00:13:57 +0100

    [Network] Transform View with Photon

--- Multiplayer/Assets/Scripts/Tank.cs
+++ Multiplayer/Assets/Scripts/Tank.cs
@@ -3,18 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using Photon.Pun;
 
 public class Tank : MonoBehaviour
 {
     private PCMovement movement;
+    private PhotonView view;
 
     private void Awake()
     {
         movement = GetComponentInChildren<PCMovement>();
+        view = GetComponent<PhotonView>();
     }
 
     private void Update()
     {
-        movement.Execute();
+        if (view.IsMine)
+            movement.Execute();
     }
 }
