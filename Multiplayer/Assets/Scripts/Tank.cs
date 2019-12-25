7caa5bc35e1dbdcd61c407a361f08742e1f017d0
Karol Sobanski <sobanskikarol91@gmail.com>
2019-12-25 14:49:21 +0100

    [Prefab] Add: Red & Green Tank

---
+++ Multiplayer/Assets/Scripts/Tank.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Tank : MonoBehaviour
+{
+    private PCMovement movement;
+
+    private void Awake()
+    {
+        movement = GetComponentInChildren<PCMovement>();
+    }
+
+    private void Update()
+    {
+        movement.Execute();
+    }
+}
