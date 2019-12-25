65c46ecf9de208b90ebc2aa6ebd30c9ab055fe29
Karol Sobanski <sobanskikarol91@gmail.com>
2019-12-25 21:26:21 +0100

    [Plugins] Add: Photon

---
+++ Multiplayer/Assets/Scripts/Networking/NetworkingManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Photon.Pun;
+
+public class NetworkingManager : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log($"We are connected to the: {PhotonNetwork.CloudRegion} ");
+    }
+}
